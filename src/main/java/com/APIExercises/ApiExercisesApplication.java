5618f759fe09804650287df2592ed483d4cf34df
Cassandrea <cassssandrea@gmail.com>
2017-04-18 15:18:32 +0800

    Added Spring files

---
+++ src/main/java/com/APIExercises/ApiExercisesApplication.java
@@ -0,0 +1,12 @@
+package com.APIExercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExercisesApplication.class, args);
+	}
+}
